ef6a17fa5b5e58ddd55d97749ee46c1c6d453eb8
gingerknight <ryanrichmond6@gmail.com>
2025-09-14 19:49:41 -0600

    readiness and refactor static files

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
+import { handlerReadiness } from "./api/readiness.js";
+
 import express from "express";
 
 const app = express();
-app.listen(8080);
+const PORT = process.env.PORT || 8080;
+
+app.use("/app", express.static("./src/app"));
+
+app.get("/healthz", handlerReadiness);
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
