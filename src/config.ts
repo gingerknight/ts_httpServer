7edd8eb17cf6b5e05abfa42c7f97f09d8d5c2c3a
gingerknight <ryanrichmond6@gmail.com>
2025-09-15 22:36:31 -0600

    setup user creation queries

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,19 @@
+process.loadEnvFile("./.env");
+
 type APIConfig = {
   fileserverHits: number;
+  dbURL: string;
 };
 
 export const config: APIConfig = {
   fileserverHits: 0,
+  dbURL: envOrThrow("DB_URL"),
 };
+
+function envOrThrow(key: string): string {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`Environment variable ${key} is not set.`);
+  }
+  return value;
+}
